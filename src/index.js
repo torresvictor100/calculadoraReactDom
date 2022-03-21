b5ec2684a4af3f9d90566648ea35890e3953402f
Victor Torres <torresvictor100@gmail.com>
2022-03-21 09:43:04 -0300

    Projeto calculadora react-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculadora from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculadora</h1>
+      <Calculadora/>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
